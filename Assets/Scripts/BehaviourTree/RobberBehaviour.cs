b5d5e7e8201bb784a605c3b99640417f487b7563
TavaresFr <152510118+TavaresFr@users.noreply.github.com>
2024-11-11 00:50:29 +0000

    NavMesh Movement

--- Assets/Scripts/BehaviourTree/RobberBehaviour.cs
+++ Assets/Scripts/BehaviourTree/RobberBehaviour.cs
@@ -13,6 +13,14 @@ public class RobberBehaviour : MonoBehaviour
 
     NavMeshAgent agent;
 
+    public enum ActionState
+    {
+        IDLE,
+        WORKING
+    };
+
+    ActionState state = ActionState.IDLE;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,14 +42,35 @@ public class RobberBehaviour : MonoBehaviour
 
     public Node.Status GoToDiamond()
     {
-        agent.SetDestination(diamond.transform.position);
-        return Node.Status.SUCCESS;
+        return GoToLocation(diamond.transform.position);
     }
 
     public Node.Status GoToVan()
     {
-        agent.SetDestination(van.transform.position);
-        return Node.Status.SUCCESS;
+        return GoToLocation(van.transform.position);
+    }
+
+    Node.Status GoToLocation(Vector3 destination)
+    {
+        float distanceToTarget = Vector3.Distance(destination, this.transform.position);
+
+        if(state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }
+        else if(Vector3.Distance(agent.pathEndPosition, destination) >= 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }
+        else if(distanceToTarget < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCCESS;
+        }
+
+        return Node.Status.RUNNING;
     }
 
     // Update is called once per frame
