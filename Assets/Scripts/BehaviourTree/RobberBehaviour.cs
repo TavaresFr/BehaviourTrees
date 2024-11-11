80e2c6ec78f805c4eff64ad99217b22255e29ea6
TavaresFr <152510118+TavaresFr@users.noreply.github.com>
2024-11-11 00:32:41 +0000

    Leaf and Action Nodes

--- Assets/Scripts/BehaviourTree/RobberBehaviour.cs
+++ Assets/Scripts/BehaviourTree/RobberBehaviour.cs
@@ -1,32 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RobberBehaviour : MonoBehaviour
 {
     BehaviourTree tree;
 
+    [SerializeField] GameObject diamond;
+
+    [SerializeField] GameObject van;
+
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+
         tree = new BehaviourTree();
         Node steal = new Node("Steal Something");
-        Node goToDiamond = new Node("Go To Diamond");
-        Node goToVan = new Node("Go To Van");
+        Leaf goToDiamond = new Leaf("Go To Diamond", GoToDiamond);
+        Leaf goToVan = new Leaf("Go To Van", GoToVan);
 
         steal.AddChild(goToDiamond);
         steal.AddChild(goToVan);
         tree.AddChild(steal);
 
-        Node eat = new Node("Eat Something");
-        Node pizza = new Node("Go To Pizza Shop");
-        Node buy = new Node("Buy Pizza");
+        tree.PrintTree();
 
-        eat.AddChild(pizza);
-        eat.AddChild(buy);
-        tree.AddChild(eat);
+        tree.Process();
+    }
 
-        tree.PrintTree();   
+    public Node.Status GoToDiamond()
+    {
+        agent.SetDestination(diamond.transform.position);
+        return Node.Status.SUCCESS;
+    }
+
+    public Node.Status GoToVan()
+    {
+        agent.SetDestination(van.transform.position);
+        return Node.Status.SUCCESS;
     }
 
     // Update is called once per frame
